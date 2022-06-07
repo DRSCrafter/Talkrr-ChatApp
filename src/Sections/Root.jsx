fe258c9e1925486a5a4a7f668af247e21c3705d7
Odysseus <drsprogramming2020@gmail.com>
2022-06-07 23:45:57 +0430

    Finalize window customization

--- src/Sections/Root.jsx
+++ src/Sections/Root.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import {IconButton} from "@mui/material";
 import CloseIcon from '@mui/icons-material/Close';
 
+function closeWindow() {
+    window.__TAURI__.process.exit();
+}
+
 function Root({children}) {
     return (
         <div style={{overflow: 'hidden'}}>
@@ -19,8 +23,8 @@ function Root({children}) {
                     height: 30,
                     backgroundColor: 'rgba(0,0,0,0.39)'
                 }}>
-                    <span className="draggable" style={{width: '100%', height: 30, position: "absolute", zIndex: 10}}/>
-                    <IconButton className="not-draggable" style={{width: 30, height: 30, color: 'red'}}>
+                    <span data-tauri-drag-region style={{width: '100%', height: 30, position: "absolute", zIndex: 10}}/>
+                    <IconButton onClick={closeWindow} className="not-draggable" style={{width: 30, height: 30, color: 'red'}}>
                         <CloseIcon style={{color: 'white', position: 'absolute', zIndex: 11}} fontSize="small"/>
                     </IconButton>
                 </span>
